1b849dbf629d7a002fcbcf4ec1bf3a94ef4505f5
Sai Krishna - Chinnu <psaikrishna42@gmail.com>
2019-08-22 22:49:39 +0530

    content file changes
    content code updated

--- content.js
+++ content.js
@@ -6,17 +6,9 @@ document.onreadystatechange = function () {
     } else if (document.readyState == "complete" && document.URL == "https://service2.diplo.de/rktermin/extern/choose_category.do?locationCode=banga&realmId=210&categoryId=337") {
         document.getElementsByTagName("a")[3].click();
     } else if (document.readyState == "complete" && document.URL == "https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=banga&realmId=210&categoryId=337") {
-        debugger //just to check the from data is available here or not!! 
-        let passportNumber;
-        chrome.storage.sync.get("entereddata", function (data) {
-            passportNumber = data.entereddata.passportno;
-            $("#appointment_captcha_month_captchaText").val(passportNumber);
-        });
-        
-        // if (JSON.parse(localStorage.getItem('formData'))) {
-        //     // $("#appointment_captcha_month_captchaText").val(JSON.parse(localStorage.getItem('formData')).passportno);
-            
-        // }
+
+        $("#appointment_captcha_month_captchaText").select();
+        checkAppointmentAvailability();
     }
     // else if (document.readyState == "complete" && document.URL == "https://service2.diplo.de/rktermin/extern/appointment_showMonth.do") {
     //     alert("No Appointment page!!!")
@@ -30,50 +22,47 @@ document.onreadystatechange = function () {
     //         document.getElementsByTagName("a")[3].click();
     //     }
     // }
-    // else if (document.readyState == "complete" && document.URL.includes("https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=banga&realmId=210&categoryId=337&dateStr=")) {
-    //     alert("from captch error page !!!")
-    //     if (document.getElementsByTagName("a")[3]) {
-    //         document.getElementsByTagName("a")[3].click();
-    //     }
-    // }
-    // else if (document.readyState == "complete" && document.URL.includes("https://service2.diplo.de/rktermin/extern/appointment_showDay.do")) {
-    //     alert("last but one page!!")
-    //     if (document.getElementsByTagName("a")[7]) {
-    //         document.getElementsByTagName("a")[7].click();
-    //     }
-    // }
+    else if (document.readyState == "complete" && (document.URL.includes("https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=banga&realmId=210&categoryId=337&dateStr=")
+        || document.URL == "https://service2.diplo.de/rktermin/extern/appointment_showMonth.do"
+        || document.URL.includes("https://service2.diplo.de/rktermin/extern/appointment_showMonth.do;jsessionid="))) {
+
+        checkAppointmentAvailability();
+    }
+    else if (document.readyState == "complete" && document.URL.includes("https://service2.diplo.de/rktermin/extern/appointment_showDay.do")) {
+        if (document.getElementsByTagName("a")[7]) {
+            document.getElementsByTagName("a")[7].click();
+        }
+    }
     else if (document.readyState == "complete" && document.URL.includes("https://service2.diplo.de/rktermin/extern/appointment_showForm.do?locationCode=banga&realmId=210&categoryId=337&dateStr=")) {
+        chrome.storage.sync.get("formData", function (data) {
+            $("#appointment_newAppointmentForm_lastname").val(data.formData.lname);
+            $("#appointment_newAppointmentForm_firstname").val(data.formData.fname);
+            $("#appointment_newAppointmentForm_email").val(data.formData.email);
+            $("#appointment_newAppointmentForm_emailrepeat").val(data.formData.email);
+            $("#appointment_newAppointmentForm_fields_0__content").val(data.formData.passportno);
+            $("#appointment_newAppointmentForm_fields_1__content").val(data.formData.phoneno);
 
-        debugger
-        chrome.storage.sync.get("entereddata", function (data) {
-            $("#appointment_newAppointmentForm_lastname").val(data.entereddata.lname);
-            $("#appointment_newAppointmentForm_firstname").val(data.entereddata.fname);
-            $("#appointment_newAppointmentForm_email").val(data.entereddata.email);
-            $("#appointment_newAppointmentForm_emailrepeat").val(data.entereddata.repeatemail);
-            $("#appointment_newAppointmentForm_fields_0__content").val(data.entereddata.passportno);
-            $("#appointment_newAppointmentForm_fields_1__content").val(data.entereddata.phoneno);
-            
         });
-        // if (JSON.parse(localStorage.getItem('formData'))) {
-        //     //To auto fill the data, form data from the popup needs to be saved before navigating to this website 
-        //     $("#appointment_newAppointmentForm_lastname").val(JSON.parse(localStorage.getItem('formData')).lname);
-        //     $("#appointment_newAppointmentForm_firstname").val(JSON.parse(localStorage.getItem('formData')).fname);
-        //     $("#appointment_newAppointmentForm_email").val(JSON.parse(localStorage.getItem('formData')).email);
-        //     $("#appointment_newAppointmentForm_emailrepeat").val(JSON.parse(localStorage.getItem('formData')).repeatemail);
-        //     $("#appointment_newAppointmentForm_fields_0__content").val(JSON.parse(localStorage.getItem('formData')).passportno);
-        //     $("#appointment_newAppointmentForm_fields_1__content").val(JSON.parse(localStorage.getItem('formData')).phoneno);
-        //     // After everything got feeded then the below method will get executed ;
-        // }
 
+    };
+    function checkAppointmentAvailability() {
+        if (document.getElementsByTagName('h2')[1] != undefined) {
+            let date = new Date()
+            let current_date = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+            let selected_date = new Date(parseInt(document.getElementsByTagName('h2')[1].innerText.trim().split('/')[1]),
+                parseInt(document.getElementsByTagName('h2')[1].innerText.trim().split('/')[0]), 1);
+            if (document.getElementsByTagName("a")[4].href == "https://service2.diplo.de/rktermin/extern/dsgvo.do?request_locale=en" && monthsDifference(selected_date, current_date) < 3) {
+                document.getElementsByTagName("a")[3].click();
+            } else if (document.getElementsByTagName("a")[4].href == "https://service2.diplo.de/rktermin/extern/dsgvo.do?request_locale=en" && monthsDifference(selected_date, current_date) == 3) {
+                alert('No Appointments Available for next 3 months!!!')
+            } else {
+                document.getElementsByTagName("a")[4].click();
+            }
+        }
+        function monthsDifference(dt2, dt1) {
+            var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+            diff /= (60 * 60 * 24 * 7 * 4);
+            return Math.abs(Math.round(diff));
+        };
     }
 };
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    chrome.storage.sync.get("entereddata", function (data) {
-        console.log(data);
-    })
-    // if (request.todo === "submitForm") {
-    //     localStorage.setItem('formData', JSON.stringify(request.entereddata));
-    //     // alert(request.entereddata.fname);
-    // };
-});
--- eventpage.js
+++ eventpage.js
@@ -1,2 +1,3 @@
 // window.open("https://service2.diplo.de/rktermin/extern/choose_realmList.do?locationCode=banga&request_locale=en");
-// window.open("https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=banga&realmId=210&categoryId=337");
+window.open("https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=banga&realmId=210&categoryId=337");
+// window.open("https://www.google.com");
